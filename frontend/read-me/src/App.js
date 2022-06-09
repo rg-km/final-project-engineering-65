b7659865e16ce0b8c72e57f3ab425cb9a9998230
cicelpupuy27 <cicelypatricia@gmail.com>
2022-06-09 18:35:47 +0700

    setup project FE

--- frontend/read-me/src/App.js
+++ frontend/read-me/src/App.js
@@ -2,11 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello world</p>
-      </header>
-    </div>
+        <h1>Hello world</h1>
   );
 }
 
---
+++ frontend/read-me/src/components/landing-page/LandingPage.js
@@ -0,0 +1,4 @@
+import React from "react";
+import {useEffect, useState} from "react";
+
+export default LandingPage;

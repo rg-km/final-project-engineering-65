150d263e331e87fcf09da8a08a7b295808ec8dd5
Nisa Alivia <nisa.alivia28@gmail.com>
2022-06-21 00:03:28 +0700

    chore: added profile page

--- frontend/read-me/src/pages/Home.js
+++ frontend/read-me/src/pages/Home.js
@@ -6,9 +6,9 @@ import Footer from "../components/Footer";
 function Home(){
     return(
         <>
-        <Navbar/>
-        <Header/>
-        <Footer/>
+            <Navbar />
+            <Header />
+            <Footer />
         </>
     )
 }

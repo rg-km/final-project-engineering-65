30bf286d57f570555eec334c2409e0c7a97b305a
cicelpupuy27 <cicelypatricia@gmail.com>
2022-06-21 19:41:41 +0700

    add read book page

--- frontend/read-me/src/App.js
+++ frontend/read-me/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Home from './pages/Home';
+import ReadBook from './pages/ReadBook';
 
 function App() {
   return (
-      <Home/>
+      // <Home/>
+      <ReadBook/>
   );
 }
 

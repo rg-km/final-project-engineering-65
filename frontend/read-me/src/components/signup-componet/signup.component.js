5bbe8d67fe224c30cf393762138df1bdc8f032f0
Nisa Alivia <nisa.alivia28@gmail.com>
2022-06-25 21:44:26 +0700

    integrate register

--- frontend/read-me/src/components/signup-componet/signup.component.js
+++ frontend/read-me/src/components/signup-componet/signup.component.js
@@ -1,118 +1,138 @@
 import React, { useState, Component, setDate } from 'react'
 import logo from '../../images/logo2.png'
+import axios, { Axios } from 'axios'
 
-export default class SignUp extends Component {
-    render() {
+// https://62b638f842c6473c4b40ff48.mockapi.io/api/read-me/
 
-        function Registration(){
-            const [date, setDate]=useState();
+function SignUp() { 
+    const [date, setDate] = useState();
+    const [data, setData] = useState({
+        email: "",
+        password: "",
+        confirm_pass: "",
+        nama: "",
+        tgl_lahir: "",
+        jenis_kelamin: ""
+    });
+
+    function registration(e){ 
+        e.preventDefault()
+        if(data.password === data.confirm_pass) {
+            delete data.confirm_pass
+            axios.post(`https://62b638f842c6473c4b40ff48.mockapi.io/api/read-me/users`, data)
+            .then((res) => {
+                console.log(res)
+                setData({
+                    email: "",
+                    password: "",
+                    confirm_pass: "",
+                    nama: "",
+                    tgl_lahir: "",
+                    jenis_kelamin: ""
+                })
+            })
+            .catch((e) => {
+                console.log(e)
+            })
+        } else {
+            alert('Password tidak sesuai')
         }
-        return (
-            <>
-            <div className='container kotak'>
-                <div className='row'>
-                    <div className='col-12 ps-1 pt-2'>
-                        <img src={logo} width="150" />
-                    </div>
-                </div>
-
-                <div className='row d-flex pt-5 justify-content-center'>
-                <div className='col-9'>
-                <form className="formlogin Buat Akun ">
-                    {/* <h5 className='title_u'></h5> */}
-                    <h3 className='mb-2'>Buat Akun</h3>
-                    {/* <h6 className='mb-4 text-dark'>sudah ada akun? <a href='/sign-in'>login</a></h6> */}
-                    <div className="mb-4">
-                        {/* <label>Email</label> */}
-                        <input
-                            type="email"
-                            className="mail"
-                            placeholder="Email"
-                        />
-                    </div>
-                    <div className="mb-4">
-                        {/* <label>Password</label> */}
-                        <input
-                            type="password"
-                            className="password"
-                            placeholder="Password"
-                        />
-                    </div>
-                    <div className="mb-4">
-                        {/* <label>Konfimasi Password</label> */}
-                        <input
-                            type="password"
-                            className="password "
-                            placeholder="Konfimasi Password"
-                        />
-                    </div>
-                    <div className="mb-4">
-                        {/* <label>Nama Lengkap</label> */}
-                        <input
-                            type="nama lengkap"
-                            className="user"
-                            placeholder="Nama Lengkap"
-                        />
-                    </div>
-                    <div className="mb-4">
-                        {/* <label>Tempat,Tanggal Lahir</label> */}
-                        <input
-                            type="date"
-                            onChange={e=>setDate(e.target.value)}
-                            className="date"
-                            placeholder="Tempat,Tanggal Lahir"
-                        />
-                    </div>
-                    <div className="mb-4">
-                        {/* <label>Jenis Kelamin</label> */}
-                        <input
-                            type="jenis kelamin"
-                            className="gender jenis kelamin"
-                            placeholder="Jenis Kelamin"
-                        />
-                    </div>
-
-                    {/*  <div className="mb-3">
-                        <label>Email</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Email"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label>Password</label>
-                        <input type="text" className="form-control" placeholder="Password" />
-                    </div>
-                    <div className="mb-3">
-                        <label>Konfirmasi Password</label>
-                        <input
-                            type="email"
-                            className="form-control"
-                            placeholder="Konfrimasi Password"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label>Password</label>
-                        <input
-                            type="Nama Lengkap"
-                            className="form-control"
-                            placeholder="Nama Lengkap"
-                        />
-                    </div> */}
-                    <div className="d-flex justify-content-end align-items-center mb-4">
-                        <h6 className="forgot-password pt-2 pe-4">
-                            Sudah ada akun? <a href="/sign-in" id="link">Log in</a>
-                        </h6>
-                        <button type="login" className="btn btn-success px-4">
-                            Daftar
-                        </button>
-                    </div>
-                </form>
-                </div>
+    }
+    
+    return (
+        <>
+        <div className='container kotak'>
+            <div className='row'>
+                <div className='col-12 ps-1 pt-2'>
+                    <img src={logo} width="150" />
                 </div>
             </div>
-            </>
-        )
-    }
+
+            <div className='row d-flex pt-5 justify-content-center'>
+            <div className='col-9'>
+            <form className="formlogin Buat Akun" method='post' onSubmit={registration}>
+                <h3 className='mb-2'>Buat Akun</h3>
+                <div className="mb-4">
+                    <input 
+                        onChange={e => {
+                            setData({...data,email:e.target.value})
+                        }}
+                        type="email"
+                        className="mail text-black"
+                        placeholder="Email"
+                        value={data.email}
+                    />
+                </div>
+                <div className="mb-4">
+                    <input
+                        onChange={e => {
+                            setData({...data,password:e.target.value})
+                        }}
+                        type="password"
+                        className="password text-black"
+                        placeholder="Password"
+                        value={data.password}
+                    />
+                </div>
+                <div className="mb-4">
+                    <input
+                        onChange={e => {
+                            setData({...data,confirm_pass:e.target.value})
+                        }}
+                        type="password"
+                        className="password text-black"
+                        placeholder="Konfimasi Password"
+                        value={data.confirm_pass}
+                    />
+                </div>
+                <div className="mb-4">
+                    <input
+                        onChange={e => {
+                            setData({...data,nama:e.target.value})
+                        }}
+                        type="nama lengkap"
+                        className="user text-black"
+                        placeholder="Nama Lengkap"
+                        value={data.nama}
+                    />
+                </div>
+                <div className="mb-4">
+                    <input
+                        onChange={e => {
+                            setData({...data,tgl_lahir:e.target.value})
+                        }}
+                        type="date"
+                        className="date text-black"
+                        placeholder="Tanggal Lahir"
+                        value={data.tgl_lahir}
+                    />
+                </div>
+                <div className="mb-4">
+                    <input
+                        onChange={e => {
+                            setData({...data,jenis_kelamin:e.target.value})
+                        }}
+                        type="jenis kelamin"
+                        className="gender jenis kelamin text-black"
+                        placeholder="Jenis Kelamin"
+                        value={data.jenis_kelamin}
+                    />
+                </div>
+
+                <div className="d-flex justify-content-end align-items-center mb-4">
+                    <h6 className="forgot-password pt-2 pe-4">
+                        Sudah ada akun? <a href="/sign-in" id="link">Log in</a>
+                    </h6>
+                    <button type="submit" className="btn btn-success px-4">
+                        Daftar
+                    </button>
+                </div>
+            </form>
+            </div>
+            </div>
+        </div>
+        </>
+    )
 }
+
+export default SignUp

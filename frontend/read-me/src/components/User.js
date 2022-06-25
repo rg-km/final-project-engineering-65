dea43ebd5422d8d252ffe379247b8eb75cd7d231
Nisa Alivia <nisa.alivia28@gmail.com>
2022-06-26 00:48:36 +0700

    feat: change profile picture by gender

--- frontend/read-me/src/components/User.js
+++ frontend/read-me/src/components/User.js
@@ -1,6 +1,7 @@
 import React from "react";
 import 'antd/dist/antd.css';
 import foto from "../assets/foto-profil.png";
+import foto2 from "../assets/foto-profil-pria.png"
 import coint from "../assets/coint.PNG";
 import UnlockCard from "./UnlockCard";
 
@@ -10,7 +11,7 @@ function User() {
     return(
         <div id="main">
             <h2 className="title"> My Account </h2>
-            <center><img src={foto} width={120} height={120} id="foto-profil" alt=""/></center>
+            <center><img src={profil?.jenis_kelamin?.toLowerCase() == 'perempuan'?foto:foto2} width={120} height={120} id="foto-profil" alt=""/></center>
             <h4 className="nama">{profil?.nama}</h4>
 
             <div id="point">

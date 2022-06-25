6bf9d36accea858f92208f48de05e43cfc17bffc
Nisa Alivia <nisa.alivia28@gmail.com>
2022-06-25 14:07:44 +0700

    add unlock modal

--- frontend/read-me/src/App.js
+++ frontend/read-me/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Routes, Route } from 'react-router-dom'
 import Login from './components/login-component/login.component'
 import SignUp from './components/signup-componet/signup.component'
-import logo from './images/logo2.png'
 import Home from './pages/Home';
 import Profil from './pages/Profil';
 import ReadPage from './pages/ReadPage';

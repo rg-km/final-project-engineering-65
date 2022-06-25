e43868c7d5c0a53ef872119faa6338c1e425a1e7
Nisa Alivia <nisa.alivia28@gmail.com>
2022-06-25 14:34:42 +0700

    add logic unlock button to open unlock modal

--- frontend/read-me/src/App.js
+++ frontend/read-me/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import { BrowserRouter as Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Login from './components/login-component/login.component'
 import SignUp from './components/signup-componet/signup.component'
+import logo from './images/logo2.png'
 import Home from './pages/Home';
 import Profil from './pages/Profil';
 import ReadPage from './pages/ReadPage';
--- frontend/read-me/src/components/Modals/UnlockModal.js
+++ frontend/read-me/src/components/Modals/UnlockModal.js
@@ -2,6 +2,7 @@ import React from "react";
 import './Modal.css';
 import book1 from "../../assets/book1.jpg";
 import logo2 from "../../assets/logo-footer.png";
+import coint from "../../assets/coint.PNG";
 
 function UnlockModal({onClose}){
     return(
@@ -11,15 +12,23 @@ function UnlockModal({onClose}){
                 <div className="modalImage">
                     <img src={book1} alt=""/>
                 </div>
-                <div className="buttonRead">
-                    <button><img src={logo2} alt=""/></button>
-                </div>
+                
                 <div className="body">
                     <h5> Judul Buku </h5>
+                    
+                    <div id="point">
+                        <h4 className="coint"><img src={coint} width={35} height={30} alt=""/> 100</h4>
+                    </div>
+
+                    <div className="buttonUnlock">
+                        <button>TUKAR POINT</button>
+                    </div>
+
                     <p> Nama Penulis Buku </p>
                     <p> Nama Penerbit </p>
                     <p> Tahun Terbit </p>
                 </div>
+
                 <div className="bookSynopsis">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus ex dui, a luctus sapien tempus et. Phasellus tincidunt purus sit amet massa congue, et suscipit odio pulvinar. Ut iaculis justo augue, ut lacinia lectus maximus luctus. Orci varius natoque penatibus et magnis dis parturient montes.</p>
                 </div>
